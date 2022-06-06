45e18fba09c4c35e606f81b11267604fabfdea91
Diego García <diegomysterio@gmail.com>
2022-06-06 14:47:04 -0500

    add doc and user manual

---
+++ target/debug/deps/execute-d0a758b26bc0fd86.d
@@ -0,0 +1,6 @@
+/Users/diegogarciarodriguezdelcampo/Desktop/8vo/Tabby/target/debug/deps/execute-d0a758b26bc0fd86: src/execute/main.rs src/execute/tests.rs
+
+/Users/diegogarciarodriguezdelcampo/Desktop/8vo/Tabby/target/debug/deps/execute-d0a758b26bc0fd86.d: src/execute/main.rs src/execute/tests.rs
+
+src/execute/main.rs:
+src/execute/tests.rs:
